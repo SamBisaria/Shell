cdf1b94e22f69b21b1ca1007f2667d4298721523
spbisaria <sbisaria@unc.edu>
2024-09-28 15:48:38 -0400

    Fix memory leak

--- shell.c
+++ shell.c
@@ -5,151 +5,78 @@
 #include "shell.h"
 
 void alloc_mem_for_argv(command_t *p_cmd) {
-    int argc = p_cmd->argc;
-    p_cmd->argv = (char **) malloc((argc + 1) * sizeof(char *));
-
-    if (p_cmd->argv == NULL) {
-        return;
+    p_cmd->argv = malloc((p_cmd->argc + 1) * sizeof(char *));
+    for (int i = 0; i < p_cmd->argc; i++) {
+        p_cmd->argv[i] = malloc(MAX_ARG_LEN * sizeof(char));
     }
-
-    for (int i = 0; i < argc; i++) {
-        p_cmd->argv[i] = (char *) malloc(MAX_ARG_LEN * sizeof(char));
-
-        if (p_cmd->argv[i] == NULL) {
-            return;
-        }
-    }
-
-    p_cmd->argv[argc] = NULL;
+    p_cmd->argv[p_cmd->argc] = NULL;
 }
 
 void cleanup(command_t *p_cmd) {
-    if (p_cmd->argv != NULL) {
-        for (int i = 0; p_cmd->argv[i] != NULL; i++) {
-            free(p_cmd->argv[i]);
-        }
-        free(p_cmd->argv);
+    for (int i = 0; i < p_cmd->argc; i++) {
+        free(p_cmd->argv[i]);
+        p_cmd->argv[i] = NULL;
     }
+    free(p_cmd->argv);
     p_cmd->argv = NULL;
-    p_cmd->argc = 0;
 }
 
 void parse(char *line, command_t *p_cmd) {
-    if (line == NULL) {
-        p_cmd->argc = 0;
-        p_cmd->argv = (char **) malloc(sizeof(char *));
-        if (p_cmd->argv == NULL) {
-            return;
-        }
-        p_cmd->argv[0] = NULL;
-        return;
-    }
-
-    char *line_copy = strdup(line);
-    if (line_copy == NULL) {
-        return;
-    }
-
+    char *clone = strdup(line);
+    char *token = strtok(clone, " ");
     int argc = 0;
-    char *token = strtok(line_copy, " ");
     while (token != NULL) {
         argc++;
         token = strtok(NULL, " ");
     }
-
     p_cmd->argc = argc;
-    p_cmd->argv = (char **) malloc((argc + 1) * sizeof(char *));
-    if (p_cmd->argv == NULL) {
-        free(line_copy);
-        return;
-    }
-
-    line_copy = strdup(line);
-    if (line_copy == NULL) {
-        free(p_cmd->argv);
-        return;
-    }
-
-    int i = 0;
-    token = strtok(line_copy, " ");
-    while (token != NULL) {
-        p_cmd->argv[i] = strdup(token);
-        if (p_cmd->argv[i] == NULL) {
-            for (int j = 0; j < i; j++) {
-                free(p_cmd->argv[j]);
-            }
-            free(p_cmd->argv);
-            free(line_copy);
-            return;
-        }
-        i++;
+    alloc_mem_for_argv(p_cmd);
+    clone = strdup(line);
+    token = strtok(clone, " ");
+    for (int i = 0; i < argc; i++) {
+        strcpy(p_cmd->argv[i], token);
         token = strtok(NULL, " ");
     }
-    p_cmd->argv[argc] = NULL;
-    free(line_copy);
+    free(clone);
 }
 
 bool find_full_path(command_t *p_cmd) {
-    char *path_env = getenv("PATH");
-    if (path_env == NULL) {
-        return false;
-    }
-
-    char *path_copy = strdup(path_env);
-    if (path_copy == NULL) {
-        return false;
-    }
-
-    char *dir_path = strtok(path_copy, ":");
-    while (dir_path != NULL) {
-        char *full_path =
-            (char *) malloc(strlen(dir_path) + strlen(p_cmd->argv[0]) + 2);
-        if (full_path == NULL) {
-            free(path_copy);
-            return false;
-        }
-
-        sprintf(full_path, "%s/%s", dir_path, p_cmd->argv[0]);
+    char *path = getenv("PATH");
+    char *clone = strdup(path);
+    char *token = strtok(clone, ":");
+    while (token != NULL) {
+        char full_path[1024];
+        strcpy(full_path, token);
+        strcat(full_path, "/");
+        strcat(full_path, p_cmd->argv[0]);
         if (access(full_path, F_OK) == 0) {
-            if (p_cmd->argv[0] != NULL) free(p_cmd->argv[0]);// free the original memory
-            p_cmd->argv[0] = full_path;
-            free(path_copy);
+            strcpy(p_cmd->argv[0], full_path);
+            free(clone);
             return true;
-        } else {
-            free(full_path);
         }
-
-        dir_path = strtok(NULL, ":");
+        token = strtok(NULL, ":");
     }
-
-    free(path_copy);
+    free(clone);
     return false;
 }
+
 int execute(command_t *p_cmd) {
     if (is_builtin(p_cmd)) {
         return do_builtin(p_cmd);
     }
-
     if (!find_full_path(p_cmd)) {
         printf("Command %s not found!\n", p_cmd->argv[0]);
         return ERROR;
     }
-
     pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        return ERROR;
-    } else if (pid == 0) {
+    if (pid == 0) {
         execv(p_cmd->argv[0], p_cmd->argv);
-        printf("Command %s not found!\n", p_cmd->argv[0]);
-        exit(EXIT_FAILURE);
+        exit(ERROR);
     } else {
-        int status;
-        waitpid(pid, &status, 0);
-        return SUCCESS;
+        wait(NULL);
     }
+    return SUCCESS;
 }
-
 bool is_builtin(command_t *p_cmd) {
     // Do not modify
     char *executable = p_cmd->argv[0];
